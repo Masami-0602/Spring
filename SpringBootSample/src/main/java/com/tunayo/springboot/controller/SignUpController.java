cf7bb2f32d553ff545d2fe86cd493fc681a25b82
Masami-0602 <f.m.f.3.1415926535@gmail.com>
2022-12-24 13:47:33 +0900

    6-2-1完了

--- SpringBootSample/src/main/java/com/tunayo/springboot/controller/SignUpController.java
+++ SpringBootSample/src/main/java/com/tunayo/springboot/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.tunayo.springboot.controller;
 
 import java.util.Map;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,23 +9,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.tunayo.springboot.service.UserApplicationService;
+import com.tunayo.springboot.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignUpController {
 	
 	@Autowired
 	private UserApplicationService userApplicationService;
 	
 	@GetMapping("/signup")
-	public String getSignup(Model model) {
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+	public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
+		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 		return "user/signup";
 	}
 	
 	@PostMapping("/signup")
-	public String postSingup() {
+	public String postSignup(@ModelAttribute SignupForm form) {
+		
+		log.info(form.toString());
+	
 		return "redirect:/login";
 	}
 }
--- SpringBootSample/src/main/java/com/tunayo/springboot/service/UserApplicationService.java
+++ SpringBootSample/src/main/java/com/tunayo/springboot/service/UserApplicationService.java
@@ -1,17 +1,28 @@
 package com.tunayo.springboot.service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserApplicationService {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+//	private MessageSource messageSource = new UserApplicationService.genderMap();
 
-	public Map<String, Integer> getGenderMap(){
+	public Map<String, Integer> getGenderMap(Locale locale){
 		Map<String, Integer> genderMap = new LinkedHashMap<>();
-		genderMap.put("íjê´", 1);
-		genderMap.put("èóê´", 2);
+		String male = messageSource.getMessage("male", null, locale);
+		String female = messageSource.getMessage("female", null, locale);
+		genderMap.put(male, 1);
+		genderMap.put(female, 2);
 		return genderMap;
 	}
 }
